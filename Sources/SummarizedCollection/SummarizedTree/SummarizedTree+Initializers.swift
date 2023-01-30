a17d285d3536f73a376ad1d6034ee8723e726332
Jesse Grosjean <jesse@hogbaysoftware.com>
2023-01-30 17:39:53 -0500

    Add nodes initializer

--- Sources/SummarizedCollection/SummarizedTree/SummarizedTree+Initializers.swift
+++ Sources/SummarizedCollection/SummarizedTree/SummarizedTree+Initializers.swift
@@ -27,5 +27,15 @@ extension SummarizedTree {
         self.version = root.objectIdentifier.hashValue
     }
 
+    @inlinable
+    init<C>(untracked nodes: C) where Node == C.Element, C : Collection {
+        self.root = nodes.first!
+        self.context = Context.nonTracking
+        self.version = root.objectIdentifier.hashValue
+        for each in nodes[nodes.index(after: nodes.startIndex)...] {
+            concat(each)
+        }        
+    }
+
 }
 
